1956605b365042764cd2d612a4b6894c077d8c55
verss21 <arjunafrizal01@gmail.com>
2025-10-23 16:31:26 +0700

    menambahkan text copyright di dalam box

--- app/src/main/java/com/example/myapplication/tolibin.kt
+++ app/src/main/java/com/example/myapplication/tolibin.kt
@@ -93,4 +93,13 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
         // Teks bawah
         Box(
             modifier = Modifier.fillMaxSize()
-        )
+        ) {
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
+    }
+}
